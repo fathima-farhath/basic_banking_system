eff0cc6b9e72160fbaadc8b5032a44ea87626d40
fathimafarhath <farhathali631gmail.com>
2023-01-13 07:53:41 +0530

    navigation to tranfer.htmlis done

--- bank_app/url.py
+++ bank_app/url.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
 path("",views.home,name='home'),
- path("customer",views.customer,name='customer'),
-# path("logout",views.logout,name='logout')
+path("customer",views.customer,name='customer'),
+path('view/<int:view_id>/',views.view,name='view')
 ]
--- bank_app/views.py
+++ bank_app/views.py
@@ -8,3 +8,8 @@ def home(request):
 def customer(request):
     details=customers.objects.all().order_by('id')
     return render(request,'customer.html',{'key':details})
+
+def view(request,view_id):
+    row=customers.objects.filter(id=view_id)
+    row1=customers.objects.exclude(id=view_id)
+    return render(request,'transfer.html',{'views':row,'dd':row1})
